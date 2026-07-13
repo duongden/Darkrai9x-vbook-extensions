b0b7cd40349167dc13865d8e3617e6349014cbd2
LongVD <longvu.uet@gmail.com>
2026-07-13 20:13:18 +0700

    chore: remove per-extension src and plugin.json files

--- mato/src/chap.js
+++
@@ -1,7 +0,0 @@
-function execute(url) {
-    let response = fetch(url);
-    if (!response.ok) return Response.error('Không tải được chương');
-
-    let chapter = response.json();
-    return Response.success(chapter.pages);
-}
--- truyenvn/src/config.js
+++
@@ -1,7 +0,0 @@
-let BASE_URL = "https://khotruyen.ac";
-try {
-    if (CONFIG_URL) {
-        BASE_URL = CONFIG_URL;
-    }
-} catch (error) {
-}
